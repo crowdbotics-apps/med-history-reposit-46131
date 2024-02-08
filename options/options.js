848dd6a484808cb5d04db352171c8b2140b885ce
Crowdbotics <team@crowdbotics.com>
2024-02-08 16:10:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "med-history-reposit-46131",
+  url: "https://med-history-reposit-46131.botics.co",
+  api: "https://med-history-reposit-46131.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
